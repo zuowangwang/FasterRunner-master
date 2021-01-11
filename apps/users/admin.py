151cc9b843c45d41763f67c5778fd4248f2aec33
tao625 <ding625yutao@163.com>
2021-01-11 18:51:13 +0800

    feat: 用户管理增加项目权限操作选择框

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -3,5 +3,24 @@ from django.contrib.auth.admin import UserAdmin
 
 from .models import User
 
+class MyUserAdmin(admin.ModelAdmin):
 
-admin.site.register(User, UserAdmin)
+    def formfield_for_manytomany(self, db_field, request, **kwargs):
+        """
+        Get a form Field for a ManyToManyField.
+        """
+        # db_field.name 本模型下的字段名称
+        if db_field.name in ('groups', 'user_permissions', 'belong_project'):
+            # 过滤
+            # kwargs["queryset"] = Tag.objects.filter(show_status=True)
+            # filter_horizontal 保持横向展示
+            from django.contrib.admin import widgets
+            kwargs['widget'] = widgets.FilteredSelectMultiple(
+                db_field.verbose_name,
+                db_field.name in self.filter_vertical
+            )
+        return super(MyUserAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
+
+    filter_horizontal = ('groups', 'user_permissions', 'belong_project',)
+
+admin.site.register(User, MyUserAdmin)
